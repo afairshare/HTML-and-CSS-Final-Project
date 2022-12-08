9be72f69e64c77d4b7e6ab450e6eb4fa5f7104d8
afairshare <120075360+afairshare@users.noreply.github.com>
2022-12-07 22:41:06 -0500

    Add files via upload

---
+++ finalproject/modifystyle.js
@@ -0,0 +1,6 @@
+<script src="https://rupikaur.com/pages/about-me"></script>
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
